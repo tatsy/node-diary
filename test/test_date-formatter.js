92925a7b28f255b03e029b3895939ca0e9843ce6
tatsy <tatsy.mail@gmail.com>
2015-01-25 15:27:47 +0900

    Add date-fomatter.js test.

---
+++ test/test_date-formatter.js
@@ -0,0 +1,9 @@
+var df = require('../lib/date-formatter');
+
+var should = require('should');
+
+describe('date-formater', function() {
+    it('format', function() {
+        df.format(0).should.equal('1970/01/01 09:00:00');
+    });
+});
