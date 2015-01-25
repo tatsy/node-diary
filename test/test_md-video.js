35aeff2c4484183ba9ded402811a17fa51f78c5d
tatsy <tatsy.mail@gmail.com>
2015-01-25 16:51:08 +0900

    Add test for md-video to improve code coverage.

--- test/test_md-video.js
+++ test/test_md-video.js
@@ -3,19 +3,23 @@ var mdv = require('../lib/md-video');
 var should = require('should');
 
 describe('md-video', function() {
-    it('format', function() {
+    it('No size specification', function() {
         mdv.marked('$[](movie.mp4)').should.equal('<video src="movie.mp4" controls></video>');
     });
 
-    it('format', function() {
+    it('Movie text is specified', function() {
+        mdv.marked('$[Movie](movie.mp4)').should.equal('<video src="movie.mp4" controls>Movie</video>');
+    });
+
+    it('Only width is specified', function() {
         mdv.marked('$[](movie.mp4 =100x)').should.equal('<video src="movie.mp4" width="100" controls></video>');
     });
 
-    it('format', function() {
+    it('Only height is specified', function() {
         mdv.marked('$[](movie.mp4 =x200)').should.equal('<video src="movie.mp4" height="200" controls></video>');
     });
 
-    it('format', function() {
+    it('Both width and height are specified', function() {
         mdv.marked('$[](movie.mp4 =100x200)').should.equal('<video src="movie.mp4" width="100" height="200" controls></video>');
     });
 });
