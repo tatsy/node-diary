676293fa35aa7b03429bbd6ccca05e9ba82dfd11
tatsy <tatsy.mail@gmail.com>
2015-01-25 15:55:24 +0900

    Fix .travis.yml.

--- test/test_date-formatter.js
+++ test/test_date-formatter.js
@@ -4,6 +4,6 @@ var should = require('should');
 
 describe('date-formater', function() {
     it('format', function() {
-        df.format(0).should.equal('1970/01/01 09:00:00');
+        df.format(0).should.equal('1970/01/01 00:00:00');
     });
 });
