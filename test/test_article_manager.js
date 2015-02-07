94a09d18f8a363d7cfe73d21711cb445b582745f
tatsy <tatsy.mail@gmail.com>
2015-02-07 13:15:45 +0900

    Revising code format.

--- test/test_article_manager.js
+++ test/test_article_manager.js
@@ -1,21 +1,22 @@
+'use strict';
+
 var am     = require('../lib/article-manager.js');
-var should = require('should');
 var assert = require('assert');
 
 describe('article-manager test', function() {
-    it('article dir', function() {
-        am.dir.should.equal('./public/articles/');
-    });
+  it('article dir', function() {
+    am.dir.should.equal('./public/articles/');
+  });
 
-    it('get article list', function() {
-        am.getList(function(err, hash) {
-            if(err) {
-                throw err;
-            }
-            assert.deepEqual(Object.keys(hash), ['2015']);
-            assert.deepEqual(Object.keys(hash['2015']), ['1']);
-            var article = hash['2015']['1'][0];
-            assert.equal(article.title, 'Article Example');
-        });
+  it('get article list', function() {
+    am.getList(function(err, hash) {
+      if (err) {
+        throw err;
+      }
+      assert.deepEqual(Object.keys(hash), [ '2015' ]);
+      assert.deepEqual(Object.keys(hash['2015']), [ '1' ]);
+      var article = hash['2015']['1'][0];
+      assert.equal(article.title, 'Article Example');
     });
+  });
 });
--- test/test_date_formatter.js
+++ test/test_date_formatter.js
@@ -1,23 +1,25 @@
+'use strict';
+
 var df = require('../lib/date-formatter');
 
 var should = require('should');
 
 describe('date-formater', function() {
-    it('simple format', function() {
-        var datestr  = '1970/01/01 00:00:00';
-        var datetime = new Date(datestr);
-        df.format(datetime.getTime()).should.equal('1970/01/01 00:00:00');
-    });
+  it('simple format', function() {
+    var datestr  = '1970/01/01 00:00:00';
+    var datetime = new Date(datestr);
+    df.format(datetime.getTime()).should.equal('1970/01/01 00:00:00');
+  });
 
-    it('simple format', function() {
-        var datestr  = '2000/02/28 14:00:00';
-        var datetime = new Date(datestr);
-        df.format(datetime.getTime()).should.equal(datestr);
-    });
+  it('simple format', function() {
+    var datestr  = '2000/02/28 14:00:00';
+    var datetime = new Date(datestr);
+    df.format(datetime.getTime()).should.equal(datestr);
+  });
 
-    it('simple format', function() {
-        var datestr  = '2015/08/31 23:59:59';
-        var datetime = new Date(datestr);
-        df.format(datetime.getTime()).should.equal(datestr);
-    });
+  it('simple format', function() {
+    var datestr  = '2015/08/31 23:59:59';
+    var datetime = new Date(datestr);
+    df.format(datetime.getTime()).should.equal(datestr);
+  });
 });
